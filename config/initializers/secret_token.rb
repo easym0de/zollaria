ec48a167752886acfb39b6e77ecdffb278947434
Mridul A. Chowdhury <mridulc@gmail.com>
2012-07-23 23:56:56 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zollaria::Application.config.secret_token = 'add5962ddd46a2312981b0984d063deda1da4433ae6cf1537919e7b79688e95fcce6369c7bceb496ffafa1d374d6f3db0ac8ac8ad78aa0a4a21d4078db8083f2'
