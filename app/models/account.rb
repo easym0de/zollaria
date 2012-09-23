ec5a7792db28d4938ca39610184234539a030da6
easym0de <mridulc@gmail.com>
2012-09-23 13:36:48 -0500

    debugger point removed

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,7 +3,6 @@ class Account < ActiveRecord::Base
    def update_balance(price)
      price = price.sub('$','').to_f
      self.balance = self.balance - price
-     debugger
      self.save!
    end
 end
