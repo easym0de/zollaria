8bc74ecfdfa789e3aaed1308793acba691b5a502
easym0de <mridulc@gmail.com>
2012-09-23 13:19:44 -0500

    migration fix

--- db/migrate/20120923181329_modify_contacts.rb
+++ db/migrate/20120923181329_modify_contacts.rb
@@ -1,11 +1,11 @@
-class ModifyContacts < ActiveRecord::Migration
+class Modifyaccounts < ActiveRecord::Migration
   def up
-    rename_column :contacts, :balance, :balance_string
-    add_column :contacts, :balance, :decimal, :precision => 8, :scale => 2
+    rename_column :accounts, :balance, :balance_string
+    add_column :accounts, :balance, :decimal, :precision => 8, :scale => 2
     
     Account.reset_column_information
     Account.find_each { |a| a.update_attribute(:balance, a.balance_string) }
-    remove_column :contacts, :balace_string
+    remove_column :accounts, :balace_string
   end
 
   def down
